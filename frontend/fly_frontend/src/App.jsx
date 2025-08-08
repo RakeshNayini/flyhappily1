4bc3f17be38215e819aaa7718af974c98622617f
RakeshNayini <nayinirakeshpatel@gmail.com>
2025-08-08 14:43:15 +0530

    components

--- frontend/fly_frontend/src/App.jsx
+++ frontend/fly_frontend/src/App.jsx
@@ -1,16 +1,16 @@
-import './App.css'
+import './App.css';
 import Heading from './components/Heading';
 import Maincomp from './components/Maincomp';
 import Footer from './components/Footer';
+
 function App() {
   return (
-    <div className='mainapp'>
-        <Heading/>
-        <Maincomp />
-        <Footer />
+    <div className="mainapp">
+      <Heading />
+      <Maincomp />
+      <Footer />
     </div>
-  )
+  );
 }
-
 
 export default App;
---
+++ frontend/fly_frontend/src/components/Maincomp.jsx
@@ -0,0 +1,10 @@
+import { hydrateRoot } from "react-dom/client";
+import '../css/Maincomp.css'
+function Maincomp(){
+    return(
+        <main className="maincomp">
+            <h1>welcome</h1>
+        </main>
+    );
+}
+export default Maincomp;
