29ebcc64ff1b3cc10287254d32d03ab0d0396707
Yarramsettysaiprasanth <saiprasanth8558@gmail.com>
2025-08-08 12:51:19 +0530

    initial commit

--- frontend/fly_frontend/src/App.jsx
+++ frontend/fly_frontend/src/App.jsx
@@ -1,35 +1,48 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
+import {browserRouter as Router,Routes} from 'react-router-dom';
+import Navbar from 
+'./components/Navbar';
+import Footer from 
+'./components/Footer';
+import Home from './pages/Home';
+import Flights from'./pages/Flights';
+import MyBookings from'./pages/MYBookings';
+import Support from'./pages/Support';
+import Admin from'./pages/Admin';
+import SignIn from'./pages/SignIn';
+import Register from'./pages/Register';
+import BookingFlight from'./pages/BookFlight';
+import'./App.css';
 
-function App() {
-  const [count, setCount] = useState(0)
-
+function App (){
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <Router>
+      <div classname="App">
+       <Navbar />
 
-export default App
+      <Routes>
+         <Route path="/Home" element=
+         {<Home />} / >
+         <Route path="/Flights" 
+         element={<Flights />} / >
+         <Route path="/My-Bookings" 
+         elemntt={<My-Booking />} />
+         <Route path="/Support" 
+         elemntt={<Support />} />
+         <Route path="/Admin" 
+         elemntt={<Admin />} />
+         <Route path="/Sign In" 
+         elemntt={<Sign In />} />
+         <Route path="/Register" 
+         elemntt={<Register />} />
+              <Route 
+              path="/booking/:flightId" 
+         element={<BookingFlight />} />
+      </Routes>
+
+      <Footer />
+    </div>
+</Router>
+  );
+}
+export default App;
