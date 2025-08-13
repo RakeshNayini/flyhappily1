b5e37048b1738f3d3473815f671d9036092b83d8
akhilsai16 <akhilnarravula44@gmail.com>
2025-08-13 10:36:54 +0530

    commit

--- backend/fly_backend/fly_backend/settings.py
+++ backend/fly_backend/fly_backend/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'firstapp.apps.FirstappConfig',
+    'dbapp',
 ]
 
 MIDDLEWARE = [
@@ -76,8 +77,13 @@ WSGI_APPLICATION = 'fly_backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+       'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'flightticket',
+        'USER': 'root',
+        'PASSWORD': 'mysql@123',
+        'HOST': 'localhost',
+        'PORT': '3306',
+
     }
 }
 
--- backend/fly_backend/fly_backend/urls.py
+++ backend/fly_backend/fly_backend/urls.py
@@ -20,4 +20,6 @@ from django.urls import path,include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('first/',include('firstapp.urls')),
+    path('db/',include('dbapp.urls')),
 ]
+
